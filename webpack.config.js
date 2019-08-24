a4b663c47faec58dbfe824c84cc8fc1308ff5564
aaronyxu <sgyxu10@hotmail.com>
2019-08-24 09:23:42 +0800

    version 1

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/js/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist/js/')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          'postcss-loader' ,
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      }
+    ]
+  }
+};
